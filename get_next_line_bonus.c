d8d6a932c804f4cbc8c4ac75db12c908e47c9ace
Sweekt <benjamin.roy69@gmail.com>
2023-11-27 13:37:34 +0100

    Added bonus (multiple fds)

---
+++ get_next_line_bonus.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 11:34:48 by beroy             #+#    #+#             */
+/*   Updated: 2023/11/27 13:32:01 by beroy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	ft_bzero(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		str[i] = 0;
+		i++;
+	}
+}
+
+char	*ft_line(int fd, int rd, char *line, char bf[OPEN_MAX][BUFFER_SIZE + 1])
+{
+	while (rd && ft_linecheck(line) == 0)
+	{
+		rd = read(fd, bf[fd], BUFFER_SIZE);
+		if (rd < 0)
+			return (ft_bzero(bf[fd]), free(line), NULL);
+		bf[fd][rd] = 0;
+		line = ft_strjoin(line, bf[fd]);
+		if (line == NULL)
+			return (NULL);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buff[OPEN_MAX][BUFFER_SIZE + 1];
+	char		*line;
+	int			readed;
+
+	readed = 1;
+	if (fd < 0)
+		return (NULL);
+	if (read(fd, NULL, 0) < 0)
+		return (ft_bzero(buff[fd]), NULL);
+	line = ft_strdup(buff[fd]);
+	if (line == NULL)
+		return (NULL);
+	line = ft_line(fd, readed, line, buff);
+	if (line == NULL)
+		return (NULL);
+	ft_buffclean(buff[fd]);
+	if (line[0] == 0)
+		return (free(line), NULL);
+	return (line);
+}
+
+/*#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+	int fd;
+	char *str;
+	size_t	i;
+
+	fd = open("test", O_RDONLY);
+	i = 1;
+	while (i != 0)
+	{
+		str = get_next_line(fd);
+		printf("%lu: %s", i, str);
+		i++;
+		if (str == NULL)
+			i = 0;
+		free(str);
+
+	}
+	return (0);
+}*/
---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 13:17:43 by beroy             #+#    #+#             */
+/*   Updated: 2023/11/27 11:52:48 by beroy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+char	*ft_strdup(char *str);
+int		ft_linecheck(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+void	ft_buffclean(char *str);
+
+#endif
